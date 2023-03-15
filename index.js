2ad83fde599400903ebe93d1d15b977c4532b5bf
Wdyffs <kiruha200146@gmail.com>
2023-03-15 17:54:13 +0300

    create catclicker Premium Pro

--- index.js
+++ index.js
@@ -59,6 +59,9 @@
     updateClicks: function () {
       ++this.getActiveCat().clickerCounter;
     },
+    saveChanges: function (changes) {
+      Object.assign(this.getActiveCat(), changes);
+    },
   };
   const controller = {
     init: function () {
@@ -78,20 +81,26 @@
     updateCatClickerCount: function () {
       model.updateClicks();
     },
+    saveCat: function (cat) {
+      model.saveChanges(cat);
+      view.render();
+    },
   };
   const view = {
     init: function () {
       catsButtonListView.init();
       catCardView.init();
+      adminPaneView.init();
       this.render();
     },
     render: function () {
       catCardView.render();
+      adminPaneView.render();
     },
   };
   const catsButtonListView = {
     init: function () {
-      const buttonsBlock = document.createElement("div");
+      const buttonsBlock = document.querySelector(".buttonsBlock");
       const cats = controller.getCats();
       for (let i = 0; i < cats.length; i++) {
         const catButton = document.createElement("button");
@@ -102,40 +111,73 @@
         });
         buttonsBlock.append(catButton);
       }
-      const app = document.getElementById("app");
-      app.append(buttonsBlock);
     },
     render: function () {},
   };
   const catCardView = {
     init: function () {
-      const catArea = document.createElement("div");
-      catArea.classList.add("catArea");
-      const catCardBlock = document.createElement("div");
-      catCardBlock.classList.add("catCard");
-      this.catImage = document.createElement("img");
-      this.catNameParagraph = document.createElement("p");
-      this.catClickCounter = document.createElement("span");
-      this.catClickCounter.classList.add("counter");
+      const catArea = document.querySelector(".catArea");
+      const catCardBlock = document.querySelector(".catCardBlock");
+      this.catImage = document.querySelector(".catImage");
+      this.catNameParagraph = document.querySelector(".catName");
+      this.catClickCounter = document.querySelector(".counter");
+
       this.catImage.addEventListener("click", () => {
         const cat = controller.getActiveCat();
         controller.updateCatClickerCount(cat.name);
         this.catClickCounter.textContent =
           controller.getActiveCat().clickerCounter;
       });
-      catCardBlock.append(this.catImage);
-      catCardBlock.append(this.catNameParagraph);
-      catCardBlock.append(this.catClickCounter);
-      catArea.append(catCardBlock);
-      const app = document.getElementById("app");
-      app.append(catArea);
     },
     render: function () {
       const cat = controller.getActiveCat();
-
       this.catImage.src = cat.imgSrc;
       this.catNameParagraph.textContent = cat.name;
       this.catClickCounter.textContent = cat.clickerCounter;
+    },
+  };
+  const adminPaneView = {
+    init() {
+      this.adminButton = document.querySelector(".adminMode");
+      this.adminPane = document.querySelector(".adminPane");
+
+      this.inputName = document.querySelector("#name");
+      this.inputUrl = document.querySelector("#url");
+      this.inputClicks = document.querySelector("#clickCount");
+
+      this.adminButton.addEventListener("click", () => {
+        this.adminPane.style.display = "block";
+      });
+
+      this.saveBtn = document.querySelector("#save");
+      this.cancelBtn = document.querySelector("#cancel");
+
+      this.cancelBtn.addEventListener("click", () => {
+        const currentCat = controller.getActiveCat();
+        this.inputName.value = currentCat.name;
+        this.inputUrl.value = currentCat.imgSrc;
+        this.inputClicks.value = currentCat.clickerCounter;
+        this.adminPane.style.display = "none";
+      });
+
+      this.saveBtn.addEventListener("click", (e) => {
+        const newCat = {
+          name: this.inputName.value,
+          imgSrc: this.inputUrl.value,
+          clickerCounter: this.inputClicks.value,
+        };
+        controller.saveCat(newCat);
+      });
+      this.render();
+    },
+    render() {
+      const updateInfo = () => {
+        const currentCat = controller.getActiveCat();
+        this.inputName.value = currentCat.name;
+        this.inputUrl.value = currentCat.imgSrc;
+        this.inputClicks.value = currentCat.clickerCounter;
+      };
+      updateInfo();
     },
   };
 
